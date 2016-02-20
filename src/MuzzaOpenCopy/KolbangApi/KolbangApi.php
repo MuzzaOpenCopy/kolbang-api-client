e7f3afc1eb17fd0ddf6e51295de31ae2e62d8a24
MuzzaOpenCopy <muzza@Serenity.local>
2016-02-20 20:47:51 +0800

    Added api endpoints
    With basic mocked testing

--- src/MuzzaOpenCopy/KolbangApi/KolbangApi.php
+++ src/MuzzaOpenCopy/KolbangApi/KolbangApi.php
@@ -8,11 +8,135 @@
 
 namespace MuzzaOpenCopy\KolbangApi;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Command\Guzzle\Description;
+use GuzzleHttp\Command\Guzzle\GuzzleClient;
 
 class KolbangApi
 {
-    public function hasCheese($bool = true)
+    /**
+     * @var string The default base URL.
+     */
+    const DEFAULT_BASE_URL = 'http://kolbang.app/api/v1';
+
+    /**
+     * Create a new GuzzleClient configured to query the postcodes.io API.
+     *
+     * @param string $baseUrl The base URL to use.
+     *
+     * @return GuzzleClient A new GuzzleClient.
+     */
+    public function create($baseUrl = self::DEFAULT_BASE_URL)
     {
-        return $bool;
+        return new GuzzleClient(new Client(), $this->getServiceDescription($baseUrl));
+    }
+
+    /**
+     * Get the service description.
+     *
+     * @param string $baseUrl The base URL.
+     *
+     * @return GuzzleServiceDescription The service description.
+     */
+    protected function getServiceDescription($baseUrl)
+    {
+        return new Description(
+            array(
+                'baseUrl' => $baseUrl,
+                'operations' => array(
+                    'cinemas' => array(
+                        'uri' => 'cinemas',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array()
+                    ),
+                    'cinema' => array(
+                        'uri' => 'cinemas/{cinema}',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array(
+                            'cinema' => array(
+                                'location' => 'uri',
+                                'description' => 'The cinema to retrieve details about.',
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'cinemaevents' => array(
+                        'uri' => 'cinemas/{cinema}/events',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array(
+                            'cinema' => array(
+                                'location' => 'uri',
+                                'description' => 'The cinema to retrieve event details about.',
+                                'required' => true
+                            )
+                        )
+                    ),
+//                    'events' => array(
+//                        'uri' => 'events',
+//                        'httpMethod' => 'GET',
+//                        'responseModel' => 'getResponse',
+//                        'parameters' => array()
+//                    ),
+                    'event' => array(
+                        'uri' => 'events/{event}',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array(
+                            'event' => array(
+                                'location' => 'uri',
+                                'description' => 'The event to retrieve details about.',
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'films' => array(
+                        'uri' => 'films',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array()
+                    ),
+                    'film' => array(
+                        'uri' => 'films/{film}',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array(
+                            'film' => array(
+                                'location' => 'uri',
+                                'description' => 'The film to retrieve details about.',
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'filmevents' => array(
+                        'uri' => 'films/{film}/events',
+                        'httpMethod' => 'GET',
+                        'responseModel' => 'getResponse',
+                        'parameters' => array(
+                            'film' => array(
+                                'location' => 'uri',
+                                'description' => 'The film to retrieve event details about.',
+                                'required' => true
+                            )
+                        )
+                    )
+                ),
+                'models' => array(
+                    'getResponse' => array(
+                        'type' => 'object',
+                        'properties' => array(
+                            'statusCode' => array(
+                                'location' => 'statusCode'
+                            )
+                        ),
+                        'additionalProperties' => array(
+                            'location' => 'json'
+                        )
+                    )
+                )
+            )
+        );
     }
 }
--- tests/KolbangTest.php
+++ tests/KolbangTest.php
@@ -1,13 +1,97 @@
 <?php
 
 use MuzzaOpenCopy\KolbangApi\KolbangApi;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Subscriber\Mock;
 
 class KolbangTest extends PHPUnit_Framework_TestCase {
 
-    public function testKolbangHasCheese()
+    /**
+     * @var string Test base URL.
+     */
+    const BASE_URL = 'http://kolbang.app/api/v1/';
+
+    /**#@+
+     * @var string A valid film id.
+     */
+    const VALID_FILMID = 1;
+    /**#@-*/
+
+    /**
+     * @var string A valid cinema id.
+     */
+    const VALID_CINEMAID = 1;
+
+    /**
+     * @var string A valid event id.
+     */
+    const VALID_EVENTID = 1;
+
+    /**
+     * @varMuzzaOpenCopy\KolbangApi\KolbangApi The ClientFactory.
+     */
+    protected $clientFactory;
+
+    /**
+     * @var GuzzleHttp\Command\Guzzle\GuzzleClient A client, created by the factory.
+     */
+    protected $client;
+
+    /**
+     * Set up.
+     */
+    public function setUp()
     {
-        $kolbang = new KolbangApi;
-        $this->assertTrue($kolbang->hasCheese());
+        $responseArray = [];
+
+        $this->clientFactory = new KolbangApi();
+        $this->client = $this->clientFactory->create(self::BASE_URL);
+
+        $mockResponse = new \GuzzleHttp\Subscriber\Mock([
+            new GuzzleHttp\Message\Response(200, [], GuzzleHttp\Stream\Stream::factory(json_encode($responseArray)))
+        ]);
+        $guzzle = $this->client->getHttpClient();
+        $guzzle->getEmitter()->attach($mockResponse);
+    }
+
+    /** @test */
+    public function factory_create_method_returns_instanceOf_client()
+    {
+        $this->assertInstanceOf('GuzzleHttp\Command\Guzzle\GuzzleClient', $this->client);
+    }
+
+        /**
+         * testClientHasCommand
+         *
+         * @param string $commandName The command name.
+         * @param array $commandArguments The command arguments.
+         *
+         * @dataProvider getCommandNamesWithArguments
+         */
+        public function testClientHasCommand($commandName, array $commandArguments)
+    {
+        $response = $this->client->$commandName($commandArguments);
+
+        $this->assertEquals(200, $response['statusCode']);
+    }
+
+        /**
+         * Get an array of command names with arguments.
+         *
+         * @return array An array, each element an array containing a command name and its arguments.
+         */
+        public function getCommandNamesWithArguments()
+    {
+        return array(
+            array('cinema', array('cinema' => self::VALID_CINEMAID)),
+            array('cinemas', array()),
+            array('cinemaevents', array('cinema' => self::VALID_CINEMAID)),
+            //array('events', array()),
+            array('event', array('event' => self::VALID_EVENTID)),
+            array('films', array()),
+            array('film', array('film' => self::VALID_FILMID)),
+            array('filmevents', array('film' => self::VALID_FILMID))
+        );
     }
 
 }
